e28c8b93edc7bfddeb571b609f94720088932db1
Don <Donny.wu4@gmail.com>
2022-10-09 22:02:44 -0400

    Added hook for text input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import Employee from './components/Employee';
-const showEmployees = true;
+
 function App() {
+  const [role, setRole] = useState('dev');
+  const showEmployees = true;
   return (
     <div className="App">
         {showEmployees ? (
           <>
+            <input
+              type = "text"
+              onChange={(e) =>{
+                console.log(e.target.value);
+                setRole(e.target.value);
+              }
+              }/>
             <Employee name="Caleb" role="Intern"/>
-            <Employee name="Dan" role= "Associate"/>
+            <Employee name="Dan" role= {role}/>
             <Employee name="Shawn"/>
           </>
           ):(
