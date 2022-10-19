745d78cbccdf23f840fbe3cd20f9ee92101cf98e
Don <Donny.wu4@gmail.com>
2022-10-19 00:21:51 -0400

    Updated EditEmployee component to allow editing name and role field

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -2,7 +2,7 @@ import EditEmployee from "./EditEmployee";
 
 function Employee(props){
     return(
-        <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <div className="min-w-[350px] max-w-[350px] m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
 
             <img className="object-cover rounded-full h-[100px] w-[100px] mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" 
                  src={props.img}
@@ -15,7 +15,7 @@ function Employee(props){
                     <p className="text-slate-500 font-medium">{props.role}</p>
                 </div>
 
-                <EditEmployee />
+                <EditEmployee name ={props.name} role={props.role}/>
 
                 <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Update</button>
             </div>
